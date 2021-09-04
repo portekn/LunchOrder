888db0201a19f93af02bd9ad6c22d385fe42fcdb
Kelli Porter <Portekn@mail.uc.edu>
2021-09-04 11:42:03 -0400

    Step 5: Text boxes and group boxes change lables accordingly.

--- LunchOrder/Form1.cs
+++ LunchOrder/Form1.cs
@@ -37,47 +37,59 @@ namespace LunchOrder
         {
             ClearTotals();
             ClearAddOns();
-            groupBox1.Text.Equals("Add-on items ($.75/each)");
-            checkBox1.Text.Equals("Lettuce, tomato, and onions");
-            checkBox2.Text.Equals("Ketchup, mustard, and mayo");
-            checkBox3.Text.Equals("French fries");
+            groupBox1.Text = "Add-on items ($.75/each)";
+            checkBox1.Text = "Lettuce, tomato, and onions";
+            checkBox2.Text = "Ketchup, mustard, and mayo";
+            checkBox3.Text = "French fries";
         }
 
         private void radioButton2_CheckedChanged(object sender, EventArgs e) //Pizza
         {
             ClearTotals();
             ClearAddOns();
-            groupBox1.Text.Equals("Add-on items ($.50/each)");
-            checkBox1.Text.Equals("Pepperoni");
-            checkBox2.Text.Equals("Sausage");
-            checkBox3.Text.Equals("Olives");
+            groupBox1.Text = "Add-on items ($.50/each)";
+            checkBox1.Text = "Pepperoni";
+            checkBox2.Text = "Sausage";
+            checkBox3.Text = "Olives";
         }
 
         private void radioButton3_CheckedChanged(object sender, EventArgs e) //Salad
         {
             ClearTotals();
             ClearAddOns();
-            groupBox1.Text.Equals("Add-on items ($.25/each)");
-            checkBox1.Text.Equals("Croutons");
-            checkBox2.Text.Equals("Bacon bits");
-            checkBox3.Text.Equals("Bread sticks");
+            groupBox1.Text = "Add-on items ($.25/each)";
+            checkBox1.Text = "Croutons";
+            checkBox2.Text = "Bacon bits";
+            checkBox3.Text = "Bread sticks";
         }
 
         //----------Methods----------//
         private void ClearTotals() //Clears the totals in the Order Total group box
         {
             //code to clear totals here
-            txtOrderTotal.Text.Equals("");
-            txtSalesTax.Text.Equals("");
-            txtSubtotal.Text.Equals("");
+            foreach (Control tBox in groupBox2.Controls)
+            {
+                if (tBox is TextBox)
+                {
+                    ((TextBox)tBox).Equals("");
+                }
+            }
+            //txtOrderTotal.Text.Equals("");
+            //txtSalesTax.Text.Equals("");
+            //txtSubtotal.Text.Equals("");
         }
 
         private void ClearAddOns() //Clears the selected add ons in the Add-on group box
         {
             //code to clear add ons here
-            checkBox1.CheckState.Equals(false);
-            checkBox2.CheckState.Equals(false);
-            checkBox3.CheckState.Equals(false);
+
+            foreach (Control cBox in groupBox1.Controls)
+            {
+                if (cBox is CheckBox)
+                {
+                    ((CheckBox)cBox).Checked = false;
+                }
+            }
         }
     }
 }
